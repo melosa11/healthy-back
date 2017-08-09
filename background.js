ce29dc1a5e91069c489a8a5e07a8c23e1bcb8c4e
David Sutor <david@sutor.sk>
2017-08-09 22:24:36 +0200

    icon-bug solved

--- background.js
+++ background.js
@@ -2,8 +2,10 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 const DEFAULT = "20";
+let isAlive = window.localStorage.getItem("alive") || "true";
 //**Restore Settings **/
 function onGet(result) {
+    liveCheck(isAlive);
     let interval = result.interval || DEFAULT;
     console.log(interval);
     createAlarm(interval);
@@ -37,4 +39,13 @@ function createAlarm(interval) {
     };
 
     browser.alarms.onAlarm.addListener(alarmListener);
+}
+
+function liveCheck(par) {
+    if(par === "true"){
+        browser.browserAction.setIcon({ path: "../icons/logo.svg" });
+    }
+    else{
+        browser.browserAction.setIcon({ path: "../icons/logo-off.svg" });
+    }
 }
